bc41d10cb1456dc2827973a5f474a663cd7a0ef4
hujie0067 <113668011+hujie0067@users.noreply.github.com>
2023-02-10 16:13:32 +0900

    Add files via upload

--- 05_tic tac toe_06_sound/js/tic_tac_toe.js
+++ 05_tic tac toe_06_sound/js/tic_tac_toe.js
@@ -43,14 +43,12 @@ let winningLine = null;
 //NewGameボタンしゅとくい
 
 //メッセージ
-const msgtxt1 = '<p class="image"><img src ="img/penguins.jpg" width=61px height=61px></p><p class="text">Penguins Attack!(you turn)</p>';
-const msgtxt2 = '<p class="image"><img src ="img/whitebear.jpg" width=61px height=61px></p><p class="text">WhiteBear Attack(computer turn)!</p>';
+const msgtxt1 = '<p class="image"><img src ="img/penguins.jpg" width=61px height=61px></p><p class="text">Penguins Attack!</p>';
+const msgtxt2 = '<p class="image"><img src ="img/whitebear.jpg" width=61px height=61px></p><p class="text">WhiteBear Attack!</p>';
 const msgtxt3 = '<p class="image"><img src ="img/penguins.jpg" width=61px height=61px></p><p class="text animate__animated animate__lightSpeedInRight">Penguins Win!!</p>';
 const msgtxt4 = '<p class="image"><img src ="img/whitebear.jpg" width=61px height=61px></p><p class="text animate__animated animate__lightSpeedInLeft">WhiteBear Win!!</p>';
 const msgtxt5 = '<p class="image"><img src ="img/penguins.jpg" width=61px height=61px><img src ="img/whitebear.jpg" width=61px height=61px></p><p class="text animate__bounceIn">Draw!!</p>';
 
-//サウンド
-let gameSound = ["sound/click_sound1.mp3","sound/click_sound2.mp3","sound/penwin_sound.mp3","sound/bearwin_sound.mp3","sound/draw_sound.mp3"];
 //JavaScriptでfilterを方法
 function JudgLie(targetArray, idArray) {
     return targetArray.filter(function(e) {
@@ -62,60 +60,59 @@ function JudgLie(targetArray, idArray) {
 window.addEventListener("DOMContentLoaded",
     function () {
         setMessage("pen-turn");
-        squaresArray.forEach(function(square){
-            square.classList.add("js-clickable");
-        })
     }, false
 );
-/*発火   */
-squaresArray.forEach(function(square) {
-    square.addEventListener('click', () => {
-        let gameOverFlg = isSelect(square); /*gameStatu*/
 
-        //GameOverflg
-        if(gameOverFlg === "0"){
-            const squaresBox = document.getElementById("squaresBox");
-            squaresBox.classList.add("js-unclickable");
-            setTimeout(
-                function(){
-                    bearTurn();
-                },
-                "2000"
-            );
-        }
-    });
+a_1.addEventListener("click",
+    function () {
+        isSelect(a_1);
+    }, false
+);
+
+
+a_2.addEventListener("click", () => {
+    isSelect(a_2);
+});
+a_3.addEventListener("click", () => {
+    isSelect(a_3);
+});
+b_1.addEventListener("click", () => {
+    isSelect(b_1);
+});
+b_2.addEventListener("click", () => {
+    isSelect(b_2);
+});
+b_3.addEventListener("click", () => {
+    isSelect(b_3);
+});
+c_1.addEventListener("click", () => {
+    isSelect(c_1);
+});
+c_2.addEventListener("click", () => {
+    isSelect(c_2);
+});
+c_3.addEventListener("click", () => {
+    isSelect(c_3);
 });
 
 
+
 function isSelect(selectSquare) {
-    let gameOverFlg = "0";
-
     if(flag === "pen-flag") {
-        let music = new Audio(gameSound[0]);
-        music.currentTime = 0;
-        music.play(); //再生
-
         selectSquare.classList.add("js-pen-checked");
         selectSquare.classList.add("js-unclickable");
-        selectSquare.classList.remove("js-clickable");
 
         //penguins Win
         if(isWinner("penguins")) {
             setMessage("pen-win");
             gameOver("penguins");
-            return gameOverFlg = "1";
+            return;
         }
         setMessage("bear-turn");
         flag = "bear-flag";
     } else {
-        //クリックサウンド
-        let music = new Audio(gameSound[1]);
-        music.currentTime = 0;
-        music.play(); //再生
-
         selectSquare.classList.add("js-bear-checked");
         selectSquare.classList.add("js-unclickable");
-        selectSquare.classList.remove("js-clickable");
         if(isWinner("bear")){
             setMessage("bear-win");
             gameOver("bear");
@@ -129,9 +126,7 @@ function isSelect(selectSquare) {
     if(counter === 0) {
         setMessage("draw");
         gameOver("draw");
-        return gameOverFlg = "1";
     }
-    return gameOverFlg = "0"
 }
 
 function isWinner(symbol) {
@@ -173,31 +168,11 @@ function setMessage(id) {
 }
 
 function gameOver(status) {
-    //GameOver サウンド
-    let w_sound // wkサウンドの種類
-    switch (status) {
-        case "penguins":
-            w_sound = gameSound[2];
-            break;
-        case "bear":
-            w_sound = gameSound[3];
-            break;
-        case "draw":
-            w_sound = gameSound[4];
-        break;
-    }
-
-    let music = new Audio(w_sound);
-    music.currentTime = 0;
-    music.play(); //再生
     // all square unclickable
-   // squaresArray.forEach(function (square) {
-        //square.classList.add("js-unclickable");
+    squaresArray.forEach(function (square) {
+        square.classList.add("js-unclickable");
 
-   // });
-   squaresBox.classList.add("js-unclickable");
-
-   newgamebtn_display.classList.remove("js-hidden");
+    });
 
     //display New Game button : display
     newgamebtn_display.classList.remove("js-hidden");
@@ -248,28 +223,10 @@ newgamebtn.addEventListener("click",function () {
         square.classList.remove("js-unclickable");
         square.classList.remove("js-pen_highLight");
         square.classList.remove("js-bear_highLight");
-        square.classList.add("js-clickable");
     });
-    squaresBox.classList.remove("js-unclickable");
     setMessage("pen-turn");
     newgamebtn_display.classList.add("js-hidden");
+
     // snowfall stop
     $(document).snowfall("clear");
- }
-)
-
-function bearTurn(){
-    let gameOverFlg = "0";
-
-    const bearSquares =squaresArray.filter(function(square){
-        return square.classList.contains("js-clickable");
-    });
-
-    let n = Math.floor(Math.random()*bearSquares.length);
-    gameOverFlg = isSelect(bearSquares[n]);
-
-    if(gameOverFlg === "0"){
-        squaresBox.classList.remove("js-unclickable");
-    }
-
-}
+});
